635b6791b9103fe94296ae6fb11e1d8d58132d44
VictoriaEGonzalez <95939497+VictoriaEGonzalez@users.noreply.github.com>
2021-12-10 17:24:08 -0500

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,6 @@
 # Group-Project
-#Final Group Projects
-#
+#Final Group Project
+#Description: from the incidents data set across all zip codes in New York City, these Jupyter notebooks achieve the following: find the top ten incident calls made to 311 for the zip code 10025, and compare the proportion of illegal parking incidents for such zip code with the global rate for all zip codes. 
 Group_Members = ("Gil Moran", "Victoria Gonzalez")
 Group_Number = 35
 Group_Uni = ["gtm2132" , "veg2114"]
